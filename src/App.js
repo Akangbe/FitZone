e2b4fac63f5c0b47aa0ffc1d32cff2fc0dafe4b8
akangbe <akangbehenry28@gmail.com>
2023-03-24 23:57:01 +0100

    fix-hero-cad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import "../src/components/Hero/Hero";
 import Hero from "../src/components/Hero/Hero";
+// import program from "./components/program/program";
+import Programs from "./components/program/program";
+// ("../src/components/program/program");
 function App() {
   return (
     <div className="App">
       <span style={{ color: "white" }}>
         <Hero />
+        <Programs />
       </span>
     </div>
   );
---
+++ src/components/program/program.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./program.css";
+// import {programsData} from "../../data/programsData"
+import {programsData} from "../../data/programsData"
+const program = () => {
+  return (
+    <div className="programs" id="programs">
+      {/* Header */}
+      <div className="programs-header">
+        <span className="stroke-text">Explore our </span>
+        <span className="">Programs</span>
+        <span className="stroke-text">to shape your body</span>
+      </div>
+
+
+      
+     <div className="program-categories">
+      {programsData.map((program)=>(
+        <div className="category" >
+
+{program.image} 
+<span >{program.heading}.</span>
+<span >{program.details}</span>
+{/* <div><div> */}
+</div>
+      ))}
+     </div>
+    </div>
+  );
+};
+
+export default program;
