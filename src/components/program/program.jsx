d2ae0476c69a986cae083b99959d7cffab45ebb8
akangbe <akangbehenry28@gmail.com>
2023-05-23 07:13:20 +0100

    1st commit in may

--- src/components/program/program.jsx
+++ src/components/program/program.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./program.css";
 // import {programsData} from "../../data/programsData"
-import {programsData} from "../../data/programsData"
+import RightArrow from "../../assets/rightArrow.png";
+import { programsData } from "../../data/programsData";
 const program = () => {
   return (
     <div className="programs" id="programs">
@@ -12,20 +13,19 @@ const program = () => {
         <span className="stroke-text">to shape your body</span>
       </div>
 
-
-      
-     <div className="program-categories">
-      {programsData.map((program)=>(
-        <div className="category" >
-
-{program.image} 
-<span >{program.heading}</span>
-<span >{program.details}</span>
-<div></div>
-</div>
-      ))}
-
-     </div>
+      <div className="program-categories">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="" srcset="" />{" "}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
