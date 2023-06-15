c42efdd61c7feef99f1f0a0c0d500a190ee33320
akangbe <akangbehenry28@gmail.com>
2023-06-15 18:20:32 +0100

    First update in june

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "../src/components/Hero/Hero";
 import Hero from "../src/components/Hero/Hero";
 // import program from "./components/program/program";
 import Programs from "./components/program/program";
+// import Reasons from "./components/Process/reasons";
+import Reason from "./components/Process/Reason";
 // ("../src/components/program/program");
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <span style={{ color: "white" }}>
         <Hero />
         <Programs />
+        {/* <Reasons /> */}
+        <Reason />
       </span>
     </div>
   );
---
+++ src/components/Process/Reason.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Reason.css";
+import tick from "../../assets/tick.png";
+import nike from "../../assets/nike.png";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import nb from "../../assets/nb.png";
+import adidas from "../../assets/adidas.png";
+
+function Reason() {
+  return (
+    <div className="reasons" id="reasons">
+      <div className="left-r">
+        <img src={image1} alt="" />
+        <img src={image2} alt="" />
+        <img src={image3} alt="" />
+        <img src={image4} alt="" />
+      </div>
+      <div className="right-r">
+        <span>Some Reason</span>
+        <div>
+          <span className="stroke-text">Why </span>
+          <span>Choose Us</span>
+        </div>
+        <div className="details-r">
+          <div>
+            <img src={tick} alt=""></img>
+            <span>100+ Expert Coauch</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>30% Discount for New Memeber</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>Reliable partner</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>Positive Result</span>
+          </div>
+        </div>
+        <span style={{ fontWeight: "bold", color: "var( --lightgray)" }}>
+          OUR PARTNERS
+        </span>
+        <div className="partner">
+          <img src={nike} alt="" />
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Reason;
