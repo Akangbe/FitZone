8d9e2e74df670728eb93a730f9b4462c6efe7438
akangbe <akangbehenry28@gmail.com>
2023-06-17 22:29:25 +0100

    FixBug

--- src/components/Plans/plans.jsx
+++ src/components/Plans/plans.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { plansData } from "../../data/plansData";
-import whiteTick from "../../assets/whiteTick.png"
+import whiteTick from "../../assets/whiteTick.png";
 import "./plan.css";
 function plans(props) {
   return (
@@ -10,36 +10,25 @@ function plans(props) {
         <span className="stroke-text">Your Journey</span>
         <span>Now within us</span>
       </div>
-      {/* <div className="plans">
-        {plansData.map((plan, i) => {
+    
+
+      <div className="plans">
+        {plansData.map((plan, i) => (
           <div className="plan" key={i}>
             {plan.icon}
             <span>{plan.name}</span>
             <span>${plan.price}</span>
-            <div className="features"></div>
+            <div className="features">
+              {plan.features.map((features, i) => (
+                <div className="feature">
+                  <img src={whiteTick} alt="" srcset="" />
+                  <span key={i}>{features}</span>
+                </div>
+              ))}
+            </div>
           </div>
-        })}
-      </div> */}
-
-<div className="plans">
-  {plansData.map((plan, i) => (
-    <div className="plan" key={i}>
-      {plan.icon}
-      <span>{plan.name}</span>
-      <span>${plan.price}</span>
-      <div className="features">
-         {plan.features.map((features, i)=>
-         <div className="feature">
-          <img src="" alt="" srcset="" />
-         </div>
-         )}
+        ))}
       </div>
-    </div>
-  ))}
-</div>
-
-
-
     </div>
   );
 }
