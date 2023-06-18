1d3cfff66b62ce09742cbf43cf7467a58e4d6177
akangbe <akangbehenry28@gmail.com>
2023-06-18 22:57:36 +0100

    add testimonial section

---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Testimonial() {
+  return (
+    <div className='Testimonial'>
+        <div className='left-t'></div>
+        <div className='left-t'></div>
+    </div>
+  )
+}
