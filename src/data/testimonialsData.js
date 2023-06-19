acf75ad061671e959335457253fadc832ca553dd
akangbe <akangbehenry28@gmail.com>
2023-06-19 22:40:05 +0100

    Fixbug

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -3,20 +3,20 @@ import image2 from "../assets/t-image2.jpg";
 import image3 from "../assets/t-image3.jpg";
 
 export const testimonialsData = [
-  // {
-  //   image: image1,
-  //   review:
-  //     "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-  //   name: "MATHEW OYENEKAN",
-  //   status: "Environmental Cleaner",
-  // },
-  // {
-  //   image: image2,
-  //   review:
-  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ",
-  //   name: "ELIJAH OLUMIDE",
-  //   status: "COACH",
-  // },
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: "MATHEW OYENEKAN",
+    status: "Environmental Cleaner",
+  },
+  {
+    image: image2,
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ",
+    name: "ELIJAH OLUMIDE",
+    status: "COACH",
+  },
   {
     image: image3,
     review:
