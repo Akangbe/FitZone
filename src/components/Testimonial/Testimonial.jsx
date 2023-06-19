c16cd7619bac8c46ec62434d91c688f85f2839bf
akangbe <akangbehenry28@gmail.com>
2023-06-19 22:55:12 +0100

    Update

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Testimonial.css"
-export default function Testimonial() {
+import { testimonialsData } from "../../data/testimonialsData";
+const Testimonial = ()=> {
+
+  const [selected, Setselected]=useState(0)
+  const tLength = testimonialsData.length;
   return (
     <div className="Testimonial">
       <div className="left-t">
         <span>Testimonial</span>
         <span className="stroke-text">Why should choose us</span>
         <span>Say about us</span>
+        <span>
+          {testimonialsData[0].review}
+        </span>
+        <span>
+          {testimonialsData[selected].name}
+        </span>
+        {testimonialsData[selected].status}
       </div>
       <div className="right-t"></div>
     </div>
   );
 }
+export default Testimonial
